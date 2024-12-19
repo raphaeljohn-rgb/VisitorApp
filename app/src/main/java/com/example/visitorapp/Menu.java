15c3ed429b5121510eeef972d038d7a018c17f81
raphaeljohn-rgb <raphaelcjohn20@gmail.com>
2024-12-19 11:47:11 +0530

    sucess

--- app/src/main/java/com/example/visitorapp/Menu.java
+++ app/src/main/java/com/example/visitorapp/Menu.java
@@ -1,6 +1,9 @@
 package com.example.visitorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Menu extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+        b1 = (Button) findViewById(R.id.view);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob = new Intent(getApplicationContext(),Viewall.class);
+                startActivity(ob);
+            }
+        });
 
 }
 }
